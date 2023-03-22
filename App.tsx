8efd73fede19c72e23af1303dd07407ba2aa22e3
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-03-22 23:16:18 +0200

    formatting added

--- App.tsx
+++ App.tsx
@@ -1,32 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Alert} from 'react-native';
+
 import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import axios from 'axios';
 
-export default function App() {
-  const [value, setValue] = useState('');
-  const inputHandler = (animal: any) => setValue(animal);
+interface IApiResponse {
+  result: string;
+}
+
+export default function App(): JSX.Element {
+  const [value, setValue] = useState<string>('');
+  const [response, setResponse] = useState<string>('');
+  const inputHandler = (animal: string) => setValue(animal);
+
   const onSubmit = async () => {
     try {
-      const response = await axios.post(
+      const gptResponse = await axios.post<IApiResponse>(
         'https://gpt-back.onrender.com/api/generate',
         {
           animal: value,
-        },
+        }
       );
-      Alert.alert('_', `${JSON.stringify(response.data)}`);
-      const data = await response.data;
+      const data = await gptResponse.data.result;
+      setResponse(data);
       console.log(data);
-      console.table(Object.entries(response.data));
-      let a;
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{value}</Text>
+      <Text>{response}</Text>
       <TextInput
         placeholder="Type text"
         value={value}
@@ -38,8 +44,6 @@ export default function App() {
     </View>
   );
 }
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -49,7 +53,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   input: {
-    width: 200,
+    width: '80%',
+    borderRadius: 10,
     height: 44,
     padding: 10,
     borderWidth: 1,
@@ -59,4 +64,3 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
 });
- 
