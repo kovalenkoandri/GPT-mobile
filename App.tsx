b5ac634f8eda9579cfae4d1c0757ad0b1e619dd4
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-03-23 21:23:00 +0200

    chatHistory.map

--- App.tsx
+++ App.tsx
@@ -7,14 +7,21 @@ import axios from 'axios';
 interface IApiResponse {
   result: string;
 }
-
+interface ChatMessage {
+  animal: string;
+  data: string;
+}
 export default function App(): JSX.Element {
-  const [value, setValue] = useState<string>('');
+  const [value, setValue] = useState<string>(''); // The type annotation :string after the variable name response specifies that the state variable should be of type
   const [response, setResponse] = useState<string>('');
+  const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]); // The interface is then used as a type annotation for the generic type parameter of the useState hook. Specifically, the useState hook is declared with an initial state value of an empty array ([]) of objects that conform to the ChatMessage interface.
   const inputHandler = (animal: string) => setValue(animal);
 
   const onSubmit = async () => {
     try {
+      // The axios.post method returns a Promise that resolves with the response data. The await keyword is used to wait for the Promise to resolve before continuing execution.
+
+      // The <IApiResponse> syntax specifies the expected response type of the API call. It is a type parameter for the post method that tells TypeScript what shape the response data should have.
       const gptResponse = await axios.post<IApiResponse>(
         'https://gpt-back.onrender.com/api/generate',
         {
@@ -23,6 +30,8 @@ export default function App(): JSX.Element {
       );
       const data = await gptResponse.data.result;
       setResponse(data);
+      setChatHistory([...chatHistory, { animal: value, data }]);
+      setValue('');
       console.log(data);
     } catch (error) {
       console.error(error);
@@ -33,6 +42,12 @@ export default function App(): JSX.Element {
     <View style={styles.container}>
       <Text>{value}</Text>
       <Text>{response}</Text>
+      {chatHistory.map((chatItem, index) => (
+        <View key={index} style={styles.chatItem}>
+          <Text style={styles.chatInput}>{chatItem.animal}</Text>
+          <Text style={styles.chatResponse}>{chatItem.data}</Text>
+        </View>
+      ))}
       <TextInput
         placeholder="Type text"
         value={value}
@@ -52,6 +67,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  chatItem: {},
+  chatInput: {},
+  chatResponse: {},
   input: {
     width: '80%',
     borderRadius: 10,
