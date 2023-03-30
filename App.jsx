33e654b05b98dcc98d5b4c08f17c18ca8ea52d08
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-03-30 15:33:48 +0300

    initial

--- App.jsx
+++ App.jsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { styles } from './styles';
-import React, { useState,
-  useEffect, } from 'react';
+import { useState } from 'react';
 import {
   View,
   Text,
@@ -11,24 +10,27 @@ import {
   Keyboard,
   TouchableWithoutFeedback,
   TouchableOpacity,
-  ActivityIndicator
+  ActivityIndicator,
 } from 'react-native';
 import axios from 'axios';
 import { SendIcon } from './assets/send';
 import { RestrictedIcon } from './assets/restricted';
-interface IApiResponse {
-  result: string;
-}
-interface ChatMessage {
-  animal: string;
-  data: string;
-}
-export default function App(): JSX.Element {
+// interface IApiResponse {
+//   result: string;
+// }
+// interface ChatMessage {
+//   animal: string;
+//   data: string;
+// }
+// export default function App(): JSX.Element {
+export default function App() {
   const [value, setValue] = useState<string>(''); // The type annotation :string after the variable name response specifies that the state variable should be of type
   const [response, setResponse] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
-  const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]); // The interface is then used as a type annotation for the generic type parameter of the useState hook. Specifically, the useState hook is declared with an initial state value of an empty array ([]) of objects that conform to the ChatMessage interface.
-  const inputHandler = (animal: string) => {
+  // const [chatHistory, setChatHistory] = useState<ChatMessage[]>([]); // The interface is then used as a type annotation for the generic type parameter of the useState hook. Specifically, the useState hook is declared with an initial state value of an empty array ([]) of objects that conform to the ChatMessage interface.
+  const [chatHistory, setChatHistory] = useState([]); // The interface is then used as a type annotation for the generic type parameter of the useState hook. Specifically, the useState hook is declared with an initial state value of an empty array ([]) of objects that conform to the ChatMessage interface.
+  // const inputHandler = (animal: string) => {
+  const inputHandler = (animal) => {
     animal.trim();
     // if (animal.length > 512) {
     //   return setValue(animal.slice(0, 512));
@@ -42,7 +44,8 @@ export default function App(): JSX.Element {
       // The axios.post method returns a Promise that resolves with the response data. The await keyword is used to wait for the Promise to resolve before continuing execution.
 
       // The <IApiResponse> syntax specifies the expected response type of the API call. It is a type parameter for the post method that tells TypeScript what shape the response data should have.
-      const gptResponse = await axios.post<IApiResponse>(
+      // const gptResponse = await axios.post<IApiResponse>(
+      const gptResponse = await axios.post(
         'https://gpt-back.onrender.com/api/generate',
         // 'http://127.0.0.1:3005/api/generate',
         {
@@ -59,24 +62,22 @@ export default function App(): JSX.Element {
       console.error(error);
     }
   };
-// useEffect(() => { // history
-//   axios
-//     .get('https://example.com/chat/history')
-//     .then(response => {
-//       setChatHistory(response.data);
-//     })
-//     .catch(error => {
-//       console.error(error);
-//     });
-// }, []);
+  // useEffect(() => { // history
+  //   axios
+  //     .get('https://example.com/chat/history')
+  //     .then(response => {
+  //       setChatHistory(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <KeyboardAvoidingView
-          behavior="height"
-        >
+        <KeyboardAvoidingView behavior="height">
           <ScrollView style={styles.scrollView}>
             {chatHistory.map((chatItem, index) => (
               <View key={index} style={styles.chatItem}>
@@ -100,11 +101,7 @@ export default function App(): JSX.Element {
                 style={styles.sendButton}
                 activeOpacity={0.6}
               >
-                {value.length === 0 ? (
-                  <RestrictedIcon />
-                ) : (
-                  <SendIcon />
-                )}
+                {value.length === 0 ? <RestrictedIcon /> : <SendIcon />}
               </TouchableOpacity>
               <View style={styles.sendButtonText}>
                 {loading && <ActivityIndicator size="large" color="#fff" />}
