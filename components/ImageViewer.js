47aca1a3fa197c1d2defc4e616f8c24ddc1ae2f4
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-04-21 13:19:25 +0300

    icons router

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,9 +1,17 @@
 import { StyleSheet, Image } from 'react-native';
 
-export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+export default function ImageViewer({
+  placeholderImageSource,
+  selectedImage,
+  setSelectedImage,
+}) {
   const imageSource =
-    selectedImage !== null ? { uri: selectedImage } : placeholderImageSource;
-
+    selectedImage !== ''
+      ? { uri: selectedImage }
+      : { uri: `data:image/png;base64,${placeholderImageSource}` };
+   
+      setSelectedImage('');
+     
   return <Image source={imageSource} style={styles.image} />;
 }
 
