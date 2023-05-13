bb689767d4eb932af7e7be39298090f193f3449e
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-05-13 23:32:55 +0300

    findBy name removed

--- utils/keyLocalRequest.js
+++ utils/keyLocalRequest.js
@@ -1,39 +1,12 @@
 import * as FileSystem from 'expo-file-system';
 import * as ada from './textAda001onAppLoad';
-import { findFileByName } from './findFileByName';
 const { StorageAccessFramework } = FileSystem;
 
 export const keyLocalRequest = async () => {
   let isTestKey = false;
   const gifDir = FileSystem.cacheDirectory + 'giphy/';
-  // const fileName = 'openAIStringKey.txt';
-  // await findFileByName(fileName)
-  //   .then(result => {
-  //     if (result) {
-  //       console.log('File found:', result);
-  //       // Perform operations on the found file
-  //     } else {
-  //       console.log('File not found');
-  //     }
-  //   })
-  //   .catch(error => {
-  //     console.error('Error while searching for file:', error);
-  //   });
 
   try {
-    // const retrievedString = await StorageAccessFramework.readAsStringAsync(
-    //   'content://com.android.externalstorage.documents/tree/primary%3AopenAi/document/primary%3AopenAi%2FopenAIStringKey.txt'
-    // );
-    // console.log(retrievedString);
-    // console.log(retrievedString.length);
-    // const retrievedDir = await StorageAccessFramework.getUriForDirectoryInRoot(
-    //   'openAi'
-    // );
-    // console.log(retrievedDir);
-    // const delDir = await FileSystem.deleteAsync(retrievedDir);
-    // 'content://com.android.externalstorage.documents/tree/primary:openAI/document/primary:openAi'
-    // );
-    // console.log(delDir);
     const retrievedString = await StorageAccessFramework.readAsStringAsync(
       gifDir + `example.txt`
     );
