f9b034aaeb442f75a6619059a90ee506c6665ba4
kovalenkoandri <kovalenkoandrii@gmail.com>
2023-05-29 16:01:02 +0300

    expo-location, reactotron, async-storage

--- App.tsx
+++ App.tsx
@@ -5,6 +5,11 @@ import { styles } from './styles';
 import { useRoute } from './router';
 import { NavigationContainer } from '@react-navigation/native';
 import { onFetchUpdateAsync } from './utils/checkUpdates';
+import Reactotron from 'reactotron-react-native';
+import * as Location from 'expo-location';
+if (__DEV__) {
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
+}
 
 SplashScreen.preventAutoHideAsync();
 export default function App(): JSX.Element {
@@ -37,7 +42,25 @@ export default function App(): JSX.Element {
   if (!appIsReady) {
     return <></>;
   }
+  // const _getLocationAsync = async () => {
+  //   // check status of location permissions
+  //   const { status } = await Location.requestForegroundPermissionsAsync();
+  //   if (status !== 'granted') {
+  //     Reactotron.log!('Permission to access location was denied');
+  //   }
+  //   // get user's position
+  //   let location = await Location.getCurrentPositionAsync({});
+  //   const lat = location.coords.latitude; // pull out latitude
+  //   const lon = location.coords.longitude; // pull out longitude
 
+  //   // display location data with Reactotron.display()
+  //   Reactotron.display!({
+  //     name: 'coordinates',
+  //     preview: `lat: ${lat}, lon: ${lon}`, // preview just the lat/lon
+  //     value: location,
+  //   });
+  // };
+  // _getLocationAsync();
   return (
     <View
       style={styles.container}
